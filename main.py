d4753e2a0d228aacee79c2612e141fc947722ae3
sayyamButt317 <sayyamButt317@gmail.com>
2026-06-17 20:00:16 +0500

    Integrate API router and enable Uvicorn server
    - Included the API router in the main FastAPI application.
    - Added Uvicorn server configuration for running the application directly.
    - Enhanced lifespan management for better application startup and shutdown.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.api.api import api_router
 from app.db.connection import close, connect
+import uvicorn
 
 
 @asynccontextmanager
@@ -11,3 +13,12 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(api_router)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
